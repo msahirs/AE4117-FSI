7bb3a62eb32a7d0d018c7676b8a43c07a7bef34d
Sahir Sujahudeen <m.s.sujahudeen@student.tudelft.nl>
2024-03-24 11:20:16 +0100

    Implemented baseline bdf2 and created loop file for it

--- assignment_3/matlab/DGCL-1D/dgcl_BDF2_loop.m
+++ assignment_3/matlab/DGCL-1D/dgcl_BDF2_loop.m
@@ -1,16 +1,29 @@
+close all
+clear all
 %% Input data
 N  = 20;        % number of cells
 
 dt   = 0.1;     % time step
 tend = 10;      % total simulation time
 
-fmt = 'b-';     % linecolor for plot of final solution
+fmt = '-b';     % linecolor for plot of final solution
 
 show_sol = 1;   % (=1) show intermediate solutions during simulation
 
-method = 1;     % method = 1 : exact at t(n+1)
+method = 2;     % method = 1 : exact at t(n+1)
                 % method = 2 : exact at t(n+1/2)
                 % method = 3 : DGCL
+
+dt_values = [0.1, 0.05, 0.025];
+for dt = dt_values
+  if dt==0.1
+    fmt = '-b';
+  elseif dt==0.05
+    fmt = '-r';
+  else
+    fmt = '-g';
+  end
+
 if (method == 1)
   disp('Mesh velocities are EXACT at t(n+1)');
 elseif (method == 2)
@@ -26,14 +39,6 @@ xi = zeros(1,N+1);  % face centers
 x  = zeros(1,N);    % cell centers
 u  = ones(1,N);     % solution
 
-dx_tn = zeros(1,N);    % cell volumes at tn
-xi_tn = zeros(1,N+1);  % face centers at tn
-u_tn  = ones(1,N);     % solution at tn
-
-dx_tnm1 = zeros(1,N);    % cell volumes at tn-1
-xi_tnm1 = zeros(1,N+1);  % face centers at tn-1
-u_tnm1  = ones(1,N);     % solution at tn-1
-
 xi  = (0:N)/N;              % face centers
 xi0 = (0:N)/N;              % face centers at t=0
 x   = (1:N)/N - 0.5/N;      % cell centers
@@ -48,13 +53,8 @@ hold off;
 
 L = zeros(N);   % Discretization matrix
 
-alpha_BE = [1 -1 0]; % Time discretization coefficients (first step Backward Euler)
-alpha_BDF = [1.5 -2 0.5]; % Time discretization coefficients (first step Backward Euler)
-alpha = [1 -1 0]; % Time discretization coefficients (first step Backward Euler)
-
 %% Simulation loop
 for t=dt:dt:tend
-
   dx_tn = dx;  % store cell volume at tn
   xi_tn = xi;  % store face center at tn
   u_tn  = u;   % store solution at tn
@@ -65,11 +65,8 @@ for t=dt:dt:tend
   
   dxidt_exnp1   = 2*pi*cos(2*pi*(t))*sin(2*pi*xi0) / N;         % exact face velocity at tn+1
   dxidt_exnp1_2 = 2*pi*cos(2*pi*(t-dt/2))*sin(2*pi*xi0) / N;    % exact face velocity at tn+1/2
-
-  %% IMPLEMENTATION OF DGCL
-  dxidt_dgcl    = (alpha(1) * xi + alpha(2) * xi_tn + alpha(3) * xi_tnm1) / dt; % face velocity satisfying D-GCL
-  %%
-
+  dxidt_dgcl    = (xi - xi_tn) / dt;                            % face velocity satisfying D-GCL
+  
   if (method == 1)
     dxidt = dxidt_exnp1;
   elseif (method ==2)
@@ -80,36 +77,28 @@ for t=dt:dt:tend
     
   % Setting up system to solve; for each cell:
   %
-  %    i=3            (u*dx)^(n+2-i)      
-  %   SUM   alpha(i) -------------- - [ u_leftface * dxidt_leftface * (-1) + u_rightface * dxidt_rightface * (+1) ] = 0
-  %    i=1                 dt                 
+  %   (u*dx)^(n+1) - (u*dx)^n      
+  %   ------------------------- - [ u_leftface * dxidt_leftface * (-1) + u_rightface * dxidt_rightface * (+1) ]^(n+1) = 0
+  %             dt                 
   %
   % which can be written for the total system as:
   %
-  % L u^(n+1) = alpha(0) * (u*dx)^n + alpha(-1) * (u*dx)^(n-1)
+  % L u^(n+1) = (u*dx)^n
   %
   % We use a simple avarage to compute the solution at a cell face:
   %   u_face = 0.5 * (u_leftcell + u_rightcell)
   %
   % Internal cells
   for i=2:N-1
-    L(i,i-1:i+1) = [0 alpha(1)*dx(i) 0] - dt * 0.5*[-dxidt(i) dxidt(i+1)-dxidt(i) dxidt(i+1)];
+    L(i,i-1:i+1) = [0 dx(i) 0] - dt * 0.5*[-dxidt(i) dxidt(i+1)-dxidt(i) dxidt(i+1)];
   end
   % Boundary cells
-  L(1,1:2)   = [alpha(1)*dx(1) 0] - dt * 0.5*[dxidt(2)-2*dxidt(1) dxidt(2)];
-  L(N,N-1:N) = [0 alpha(1)*dx(N)] - dt * 0.5*[-dxidt(N) 2*dxidt(N+1)-dxidt(N)];
+  L(1,1:2)   = [dx(1) 0] - dt * 0.5*[dxidt(2)-2*dxidt(1) dxidt(2)];
+  L(N,N-1:N) = [0 dx(N)] - dt * 0.5*[-dxidt(N) 2*dxidt(N+1)-dxidt(N)];
 
   % solve system
-  u = (L\(-alpha(2)*(dx_tn.*u_tn)'-alpha(3)*(dx_tnm1.*u_tnm1)'))';
+  u = (L\(dx_tn.*u_tn)')';
 
-  % update old variables
-  dx_tnm1 = dx_tn;
-  u_tnm1  =  u_tn;
-  xi_tnm1 = xi_tn;
-  
-  % After first time step we can use 
-  alpha= alpha_BDF;
-  
   % Show intermediate solution
   if (show_sol)
     figure(1);
@@ -128,3 +117,11 @@ title(['Solution at t=' num2str(tend)]);
 ylabel('Solution');
 xlabel('x');
 plot(x,u,fmt);
+legend(['dt=0.1'], ['dt=0.05'], ['dt=0.025']);
+
+
+end
+
+
+% saveas(figure(2), 'method_3_q21.png');
+hold off;
